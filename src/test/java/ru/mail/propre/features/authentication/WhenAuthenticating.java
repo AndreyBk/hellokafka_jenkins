73ffe2eb09f026dcc0282526f6cc38af9860733c
AndreyCh <propre@mail.ru>
2019-01-31 16:20:08 +0300

    commit 30

--- src/test/java/ru/mail/propre/features/authentication/WhenAuthenticating.java
+++ src/test/java/ru/mail/propre/features/authentication/WhenAuthenticating.java
@@ -23,7 +23,7 @@ public class WhenAuthenticating {
 
     @Test
     public void shouldBeAbleToLoginAsAdmin() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+//        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
 //        System.setProperty("webdriver.chrome.driver", "C:\\BK\\JavaProjects\\chromedriver_win32\\chromedriver.exe");
 //        System.setProperty("webdriver.gecko.driver", "C:\\BK\\JavaProjects\\geckodriver\\geckodriver.exe");
 //
@@ -33,6 +33,7 @@ public class WhenAuthenticating {
 //        FirefoxProfile myprofile = profile.getProfile("profileToolsQA");
 //
 //        WebDriver driver = new FirefoxDriver(myprofile);
+
 
 
 
