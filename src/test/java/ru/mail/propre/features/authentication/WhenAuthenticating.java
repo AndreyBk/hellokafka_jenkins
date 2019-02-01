49f2045f972055bb64589dabf9d1277219d9f988
AndreyCh <propre@mail.ru>
2019-02-01 11:14:38 +0300

    commit 30

--- src/test/java/ru/mail/propre/features/authentication/WhenAuthenticating.java
+++ src/test/java/ru/mail/propre/features/authentication/WhenAuthenticating.java
@@ -17,8 +17,8 @@ public class WhenAuthenticating {
     @Steps
     private DecohereUser _user;
 
-//    @Managed(driver = "chrome")
-    @Managed
+    @Managed(driver = "chrome")
+//    @Managed
     WebDriver _browser;
 
     @Test
